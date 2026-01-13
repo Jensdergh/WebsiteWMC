a19eb1d2257677ff4f5921c8f086a2f54e40946d
Jensdergh <jens4adam@gmail.com>
2026-01-13 21:36:03 +0100

    Add files via upload
    Beinahe fertiges Projekt

---
+++ WMC/page_1/app.js
@@ -0,0 +1,38 @@
+const questions = [
+    { text: "Elefanten fliegen gerne durch die Wüsten Pangeas", answer: true },
+    { text: "Man sollte seine Unterwäsche täglich wechseln", answer: false },
+    { text: "Das Verständnis von Kristallisationsdifferentiation ist für den Alltag unabdingbar", answer: true }
+];
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+const questionElement = document.getElementById("question");
+const scoreElement = document.getElementById("score");
+
+function Questionload() {
+    if (currentQuestionIndex < questions.length) {
+        questionElement.innerText = questions[currentQuestionIndex].text;
+    } else {
+        questionElement.innerText = "Quiz beendet!";
+        
+        const buttonsContainer = document.querySelector(".buttons");
+        if (buttonsContainer) {
+            buttonsContainer.style.display = "none";
+        }
+    }
+}
+
+function checkAnswer(userAnswer) {
+    const correctAnswer = questions[currentQuestionIndex].answer;
+
+    if (userAnswer === correctAnswer) {
+        score++;
+    }
+
+    currentQuestionIndex++;
+    scoreElement.innerText = `Score: ${score}`;
+    Questionload();
+}
+
+Questioninit();
---
+++ WMC/public.html/app.js
@@ -0,0 +1,48 @@
+const cards = document.querySelectorAll('.memory-card');
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCards() {
+    if (lockBoard || this === firstCard) return;
+
+    this.classList.add('flip'); 
+    if (!firstCard) {
+        firstCard = this; 
+        return;
+    }
+
+    secondCard = this; 
+    checkMatch();
+}
+
+function checkMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    
+    if (isMatch) {
+        
+        firstCard.removeEventListener('click', flipCards);
+        secondCard.removeEventListener('click', flipCards);
+        resetBoard();
+    } else {
+        
+        lockBoard = true;
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            resetBoard();
+        }, 1000);
+    }
+}
+
+function resetBoard() {
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
+}
+
+
+cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 18);
+    card.style.order = randomPos;
+    card.addEventListener('click', flipCards);
+});
